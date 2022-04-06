08a16a2e87a05ebb005bda1f1f7e39f8a343063e
burg113 <burg113@gmx.net>
2022-04-06 16:11:04 +0200

    input object serialisation

--- src/input/input_object.py
+++ src/input/input_object.py
@@ -2,10 +2,10 @@ import pickle
 
 
 class InputObject:
-    key_inputs = {}
-    scalar_inputs = {}
 
     def __init__(self, serialized=None):
+        self.key_inputs = {}
+        self.scalar_inputs = {}
         if serialized is not None:
             self.deserialize(serialized)
 
@@ -17,8 +17,14 @@ class InputObject:
 
     def deserialize_delta(self, data):
         key_inputs_delta, scalar_inputs_delta = pickle.loads(data)
-        for pair in key_inputs_delta:
-            self.key_inputs[pair[0]] = pair[1]
+        for key in key_inputs_delta:
+            self.key_inputs[key] = key_inputs_delta[key]
 
-        for pair in scalar_inputs_delta:
-            self.scalar_inputs[pair[0]] = pair[1]
+        for key in scalar_inputs_delta:
+            self.scalar_inputs[key] = scalar_inputs_delta[key]
+
+    def input(self, key, value, is_scalar=False):
+        if is_scalar:
+            self.scalar_inputs[key] = value
+        else:
+            self.key_inputs[key] = value
--- src/input/input_object_test.py
+++ src/input/input_object_test.py
@@ -2,20 +2,21 @@ import input_object
 
 if __name__ == "__main__":
     i = input_object.InputObject()
-    i.key_inputs["key_A"] = 1
-    i.key_inputs["key_B"] = 0
-    i.key_inputs["key_C"] = 0
+    i.input("key_A", 1)
+    i.input("key_B", 0)
+    i.input("key_C", 0)
+    i.input("key_B", 1)
+    i.input("key_B", 0)
     print(i.key_inputs)
 
     i2 = input_object.InputObject(i.serialize())
     print(i2.key_inputs)
 
     delta = input_object.InputObject()
-    delta.key_inputs["key_C"] = 1
+    print("--",delta.key_inputs)
+    delta.input("key_C", 1)
+    delta.input("key_A", 0)
     print(delta.key_inputs)
 
     i2.deserialize_delta(delta.serialize())
-
-
-
-
+    print(i2.key_inputs)

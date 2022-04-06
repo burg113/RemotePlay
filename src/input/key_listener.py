d4414c66f07bb9ad94f70d207943ab59e4fb0f76
polarfuchs <josh117f@gmail.com>
2022-04-06 17:36:04 +0200

    keyboard listener

--- src/input/key_listener.py
+++ src/input/key_listener.py
@@ -2,27 +2,28 @@ from input_object import InputObject
 from pynput import keyboard
 
 
-input_obj = InputObject()
+if __name__ == "__main__":
+	input_obj = InputObject()
 
-def on_press(key):
-	if isinstance(key, keyboard.KeyCode):
-		input_obj.input(str(key.vk), 1)
-	else:
-		input_obj.input(str(key), 1)
-	print(input_obj.key_inputs)
-	pass
+	def on_press(key):
+		if isinstance(key, keyboard.KeyCode):
+			input_obj.input(str(key.vk), 1)
+		else:
+			input_obj.input(str(key), 1)
+		print(input_obj.key_inputs)
+		pass
 
-def on_release(key):
-	if isinstance(key, keyboard.KeyCode):
-		input_obj.input(str(key.vk), 0)
-	else:
-		input_obj.input(str(key), 0)
-	print(input_obj.key_inputs)
-	pass
+	def on_release(key):
+		if isinstance(key, keyboard.KeyCode):
+			input_obj.input(str(key.vk), 0)
+		else:
+			input_obj.input(str(key), 0)
+		print(input_obj.key_inputs)
+		pass
 
-listener = keyboard.Listener(
-	on_press=on_press, 
-	on_release=on_release)
-listener.start()
+	listener = keyboard.Listener(
+		on_press=on_press, 
+		on_release=on_release)
+	listener.start()
 
-while True: pass
+	while True: pass

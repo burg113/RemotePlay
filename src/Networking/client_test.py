aa388c72af9a2bfc820471df6cff1077356755a6
burg113 <burg113@gmx.net>
2022-04-06 14:08:24 +0200

    Async Server

--- src/Networking/client_test.py
+++ src/Networking/client_test.py
@@ -4,4 +4,4 @@ HOST = "127.0.0.1"
 PORT = 5000
 
 if __name__ == "__main__":
-    networking.Client(HOST, PORT, print)
+    networking.Client(HOST, PORT)
--- src/Networking/networking.py
+++ src/Networking/networking.py
@@ -1,36 +1,67 @@
 import socket
+import uuid as unique_id
+from multiprocessing import Process
 
 
 class Server:
+    sock = None
+    callback = None
+
+    def default_callback(self, uuid, conn, data):
+        print("received:-", data, "-", "from", uuid, "no callback function configured")
+        pass
+
+    def on_new_client(self, conn, addr, uuid):
+        print(f"Connected by {addr}", uuid)
+        while True:
+            data = conn.recv(1024)
+            self.callback(uuid, conn, data)
 
     def __init__(self, host, port):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.callback = self.default_callback
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        s.bind((host, port))
-        s.listen()
-        conn, addr = s.accept()
+        sock.bind((host, port))
+        sock.listen()
 
-        with conn:
+        while True:
+            print(0)
+            conn, addr = sock.accept()
+            print(1)
+            p = Process(target=self.on_new_client, args=(conn, addr, unique_id.uuid4()))
+            print(2)
+            p.start()
+            print(3)
+
+
+"""        with conn:
             print(f"Connected by {addr}")
             while True:
                 data = conn.recv(1024)
                 if not data:
                     break
                 conn.sendall(data)
+"""
 
 
 class Client:
+    callback = None
+    sock = None
 
-    def default_callback(self):
+    def default_callback(data):
+        print("received:-", data, "- no callback function configured")
         pass
 
-    callback = default_callback
+    def __init__(self, host, port, callback=default_callback):
+        self.callback = callback
+        self.connect(host, port)
 
-    def __init__(self, host, port,callback):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((host, port))
-        s.sendall(b"Hello, world")
+    def connect(self, host, port):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((host, port))
+        print("connected")
+        sock.sendall(b"Hello, world")
 
         while True:
-            data = s.recv(1024)
-            callback(data)
+            data = sock.recv(1024)
+            self.callback(data)
